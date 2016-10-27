543994ecd01f5f4c47f4aec9498df17ec6d6e6ea
dufresnedavid <david.dufresne@savoirfairelinux.com>
2016-10-27 08:58:30 -0400

    Fix __init__.py

--- report_aeroo/__init__.py
+++ report_aeroo/__init__.py
@@ -3,7 +3,6 @@
 # © 2016 Savoir-faire Linux
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
 
-from . import report_xml
 from . import report_aeroo
 from . import models
 from . import wizard
