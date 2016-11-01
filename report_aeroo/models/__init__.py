9c999630336485c75f8243dec09180deb1e26980
dufresnedavid <david.dufresne@savoirfairelinux.com>
2016-11-01 11:07:52 -0400

    Fix model import order

--- report_aeroo/models/__init__.py
+++ report_aeroo/models/__init__.py
@@ -2,6 +2,8 @@
 # © 2016 Savoir-faire Linux
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl).
 
-from . import ir_actions_report_line
-from . import ir_actions_report_xml
-from . import report_mimetypes
+from . import (
+    report_mimetypes,
+    ir_actions_report_line,
+    ir_actions_report_xml,
+)
